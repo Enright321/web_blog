cde4a9e283cb379dc408e76ce12dfcdd9f7c5e4b
Enright321 <enrightjoe4@gmail.com>
2021-03-26 18:53:26 -0500

    second

---
+++ blog/static/blog/js/app.js
@@ -0,0 +1,12 @@
+
+// const article = document.getElementById('article');
+
+// const filter = document.getElementById('filter').addEventListener('keyup', e => {
+//   filter_items = e.target.value.toLowerCase();
+//   if (filter_items !== '') {
+//     const account = document.getElementById('profile-heading');
+//     console.log(account);
+//   } else {
+
+//   }
+// });
